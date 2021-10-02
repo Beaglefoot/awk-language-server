8851b66ee50495182a0efc22b68ddaca4fdb1c19
Beaglefoot <ch-e-st@yandex.ru>
2021-10-02 19:40:31 +0300

    Add LS from node_modules

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -13,8 +13,7 @@ let client: LanguageClient
 
 export function activate(context: ExtensionContext) {
   const serverModule = context.asAbsolutePath(
-    // path.join('node_modules', 'awk-language-server', 'out', 'server.js'),
-    path.join('..', 'server', 'out', 'server.js'),
+    path.join('node_modules', 'awk-language-server', 'out', 'server.js'),
   )
 
   // The debug options for the server
